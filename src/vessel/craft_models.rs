8a041588a587a9eaf766ff52d075d9a0fe5ad0e4
rotteegher <noisetide@gmail.com>
2023-03-30 16:46:24 +1100

    changed the interface to get Craft just with name and model name (bound to CraftTypes)

--- src/vessel/craft_models.rs
+++ src/vessel/craft_models.rs
@@ -1,13 +1,20 @@
 #[derive(Debug)]
-pub enum Cmods {
+pub enum CraftTypes {
+    Small(Smodls),
+    Medium(Mmodls),
+    Big(Bmodls),
+}
+#[derive(Debug)]
+pub enum Smodls {
     Zabuton,
     Kiff,
     Fillet,
     Typhon,
+    Pliashka,
 }
 
 #[derive(Debug)]
-pub enum Umods {
+pub enum Mmodls {
     Krishna,
     Orion,
     Pegasus,
@@ -17,9 +24,30 @@ pub enum Umods {
 }
 
 #[derive(Debug)]
-pub enum Omods{
+pub enum Bmodls {
     Stati,
     Stratus,
     Aether,
     Liubyt,
+}
+
+// Useless code, refactor later to something useful
+pub trait CraftTypesModelInterface<ANYMODEL> {
+    fn get_model(typ: ANYMODEL) -> CraftTypes;
+}
+
+impl CraftTypesModelInterface<Smodls> for Smodls {
+    fn get_model(typ: Smodls) -> CraftTypes {
+        CraftTypes::Small(typ)
+    }
+}
+impl CraftTypesModelInterface<Mmodls> for Mmodls {
+    fn get_model(typ: Mmodls) -> CraftTypes {
+        CraftTypes::Medium(typ)
+    }
+}
+impl CraftTypesModelInterface<Bmodls> for Bmodls {
+    fn get_model(typ: Bmodls) -> CraftTypes {
+        CraftTypes::Big(typ)
+    }
 }
--- src/vessel/mod.rs
+++ src/vessel/mod.rs
@@ -1,92 +1,30 @@
 use bevy::{prelude::*, window::*};
-use crate::craft_models::{Cmods::*, Omods::*, Umods::*};
 use bevy::math::vec3;
-// Plan to add more modules
+
+use craft_models::{
+*,
+CraftTypes::*,
+};
 use modules::*;
-
 pub mod craft_models;
-mod modules;
-
+pub mod modules;
 
 #[derive(Component, Debug)]
-
 // Main Vessel Information
 pub struct Craft {
     pub craft_name: String,
     pub craft_typemodel: CraftTypes,
-    // pub craft_modules: CraftModules,
 }
 
-pub use craft_models::*;
-
-// Main Vessel Types
-#[derive(Debug)]
-pub enum CraftTypes {
-    Chiisai(Cmods),
-    Chuu(Umods),
-    Ookii(Omods),
+pub trait CraftInterface<ANYMODEL> {
+    fn new_define(name: String, typ: ANYMODEL) -> Craft;
 }
-
-// Get default Craft
-impl Default for Craft {
-    fn default() -> Self {
-        Self {
-            craft_name: "Testos".to_owned(),
-            craft_typemodel: CraftTypes::Chiisai(Cmods::Zabuton),
-        }
+impl CraftInterface<Smodls> for Craft {
+    fn new_define(name: String, typ: Smodls) -> Craft {
+       Craft {
+           craft_name: name,
+           craft_typemodel: CraftTypes::Small(typ),
+       } 
     }
-}
 
-// Get Craft by specifying Model and Name
-use CraftTypes::{Chiisai, Chuu, Ookii};
-pub trait CraftGet {
-    const SIZE: CraftTypes;
-
-    fn new_def(name: String, typ: CraftTypes) -> Craft {
-        match typ {
-            CraftTypes::Chiisai(mods) => Craft {
-                craft_name: name,
-                craft_typemodel: CraftTypes::Chiisai(mods),
-            },
-            CraftTypes::Chuu(mods) => Craft {
-                craft_name: name,
-                craft_typemodel: CraftTypes::Chuu(mods),
-            },
-            CraftTypes::Ookii(mods) => Craft {
-                craft_name: name,
-                craft_typemodel: CraftTypes::Ookii(mods),
-            },
-            _ => Craft::default(),
-        }
-    }
-}
-// Make to work later
-impl CraftGet for Craft {
-    const SIZE: CraftTypes = CraftTypes::Chiisai(todo!());
-}
-
-
-// Player sprite
-pub const IMAGE: &str = "sprites/craft.png";
-
-pub fn spawncrafts(
-    mut cmd: Commands,
-    window_query: Query<&Window, With<PrimaryWindow>>,
-    asset_server: Res<AssetServer>,
-) {
-    let window = window_query.get_single().unwrap();
-    let scale: f32 = 0.15;
-
-    cmd.spawn((
-        Craft::new_def("CSX001".to_string(), Chiisai(Zabuton)),
-        SpriteBundle {
-            transform: Transform {
-                translation: vec3(0.0, 0.0, 0.0),
-                rotation: Quat::from_xyzw(0.0, 0.0, 0.0, 1.0),
-                scale: vec3(scale, scale, scale),
-            },
-            texture: asset_server.load(IMAGE),
-            ..default()
-        },
-    ));
 }
